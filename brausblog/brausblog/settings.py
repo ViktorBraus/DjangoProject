fe11be395d67d84759765645ed3ec10098e677ec
Viktorbraus <korolenkovitya92@gmail.com>
2022-10-19 13:20:02 +0300

    Added Database and Created two another users. LB№2

--- brausblog/brausblog/settings.py
+++ brausblog/brausblog/settings.py
@@ -9,9 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "app_blog"
 ]
 
 MIDDLEWARE = [
@@ -75,8 +74,10 @@ WSGI_APPLICATION = "brausblog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "brausblog_db",
+        "USER": "brausblog_usr",
+        "PASSWORD": "Koral2017",
     }
 }
 
