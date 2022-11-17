9b83fdef85f601f13c49b41b2781ce6a23b70490
Viktorbraus <korolenkovitya92@gmail.com>
2022-11-17 17:47:20 +0200

    Minor fixes

--- brausblog/app_blog/urls.py
+++ brausblog/app_blog/urls.py
@@ -5,7 +5,8 @@
 # path(r"", views.HomePageView.as_view()),
 # ]
 from django.urls import path
-
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import (HomePageView, ArticleDetail, ArticleList, ArticleCategoryList)
 
 urlpatterns = [
@@ -14,3 +15,5 @@ path(r'articles', ArticleList.as_view(), name='articles-list'),
 path(r'articles/category/<slug>', ArticleCategoryList.as_view(), name='articles-category-list'),
 path(r'articles/<year>/<month>/<day>/<slug>', ArticleDetail.as_view(), name='news-detail'),
 ]
+if settings.DEBUG:
+    urlpatterns +=   static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
